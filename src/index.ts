46b399697cb9e931eef9944b566853bc98f82338
samarmeena <sumarmeena@gmail.com>
2021-08-13 20:41:20 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-export default async function wrapper<T, U = Error>(
-  promise: Promise<T>,
-  errorExt?: Record<string, unknown>
-): Promise<[U, undefined] | [null, T]> {
-  return promise
-    .then<[null, T]>((data: T) => [null, data])
-    .catch<[U, undefined]>((err: U) => {
-      if (errorExt) {
-        Object.assign(err, errorExt);
-      }
-      return [err, undefined];
-    });
-}
+export * from "./promise wrapper";
